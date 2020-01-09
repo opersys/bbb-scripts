7d41a03576ee336f5c6c44e7a4cb080923fb97a9
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2020-01-09 17:02:16 -0500

    Changes for compiling Android 10 for BeagleBone Black.

--- build-beagleboneblack.sh
+++ build-beagleboneblack.sh
@@ -18,36 +18,35 @@ if [ $TARGET_PRODUCT != "beagleboneblack" -a $TARGET_PRODUCT != "beagleboneblack
 	exit
 fi
 
-if [ `javac -version |& cut -d " " -f 2 | cut -b 1-3` != "1.7" ]; then
-        echo "Missing JDK or not version 1.7"
-        exit
-fi
-
-echo "Building $TARGET_PRODUCT using $CORES cpu cores"
-echo ""
-echo "Building kernel"
-cd $ANDROID_BUILD_TOP/ti-kernel
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
-if [ ! -d patches/a4b ]; then
-	mkdir patches/a4b
-	cp $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/config-android-4.1 patches/defconfig 
-	if [ $? != 0 ]; then echo "ERROR"; exit; fi
-	cp  $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/*.patch patches/a4b
-	patch -p1 < $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/0001-Add-a4b-patch.patch
-	if [ $? != 0 ]; then echo "ERROR"; exit; fi
-fi
+#echo "Building $TARGET_PRODUCT using $CORES cpu cores"
+#echo ""
+#echo "Building kernel"
+#cd $ANDROID_BUILD_TOP/bb-kernel
+#if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
 AUTO_BUILD=1 ./build_kernel.sh
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-# Append the dtb to zImage because the Android build doesn't know about dtbs
-cd KERNEL
-cat arch/arm/boot/zImage arch/arm/boot/dts/am335x-boneblack.dtb > zImage-dtb
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
+# Merge an overlay for the fstab. This works around the lack of DTBO.
+rm -v am335x-boneblack-android.dtb
+(cat KERNEL/arch/arm/boot/dts/am335x-boneblack.dts && \
+     echo "#include \"../device/beagleboard/beagleboneblack/dts/$TARGET_PRODUCT.dts\"" ) | \
+    cpp -x assembler-with-cpp -E -undef -nostdinc -IKERNEL/include -IKERNEL/arch/arm/boot/dts | \
+    KERNEL/scripts/dtc/dtc -O dtb -@ --include KERNEL/arch/arm/boot/dts \
+        -Wno-unit_address_vs_reg \
+        -Wno-unit_address_format \
+        -Wno-avoid_unnecessary_addr_size \
+        -Wno-alias_paths \
+        -Wno-graph_child_address \
+        -Wno-graph_port \
+        -Wno-unique_unit_address \
+        -Wno-pci_device_reg \
+        -o am335x-boneblack-android.dtb
 
-cp zImage-dtb $ANDROID_BUILD_TOP/$DEVICE_DIR
+# Append the dtb to zImage because the Android build doesn't know about dtbs
+cat KERNEL/arch/arm/boot/zImage am335x-boneblack-android.dtb > ../zImage-dtb
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
+cp ../zImage-dtb $ANDROID_BUILD_TOP/$DEVICE_DIR
 
 # Grab all the kernel modules
 mkdir $ANDROID_BUILD_TOP/$DEVICE_DIR/modules
@@ -57,19 +56,11 @@ for f in $MODULES; do
 	if [ $? != 0 ]; then echo "ERROR"; exit; fi
 done
 
-echo "Building device tree overlays"
-cd $ANDROID_BUILD_TOP/bb.org-overlays
-make DTC=../ti-kernel/KERNEL/scripts/dtc/dtc
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
-mkdir $ANDROID_BUILD_TOP/$DEVICE_DIR/dtbo
-cp src/arm/*.dtbo $ANDROID_BUILD_TOP/$DEVICE_DIR/dtbo
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
 echo "Building U-Boot"
 cd $ANDROID_BUILD_TOP/u-boot
 # The Android prebuilt gcc fails to build U-Boot, so use the Linaro gcc which
 # was installed to build the ti-kernel
-. ../ti-kernel/.CC
+CC=/home/neumann/Android/Transtex/9.0-BeagleBone/bb-kernel/dl/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 make CROSS_COMPILE=$CC am335x_evm_config
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 make CROSS_COMPILE=$CC
