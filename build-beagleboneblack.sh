ede738990c564d0f0b5225c757d9b2a039cdb162
Francois-Denis Gonthier <francois-denis.gonthier@opersys.com>
2020-02-25 10:33:18 -0500

    Adapt CSimmonds scripts for Android 10
    Changes required to build Android 10 for BeagleBone Black.

--- build-beagleboneblack.sh
+++ build-beagleboneblack.sh
@@ -1,80 +1,86 @@
 #!/bin/bash
 
-DEVICE_DIR=device/beagleboard/beagleboneblack
-
 # The number of CPU cores to use for Android compilation. Default is
 # all of them, but you can override by setting CORES
 if [ -z $CORES ]; then
-	CORES=$(getconf _NPROCESSORS_ONLN)
+    CORES=$(getconf _NPROCESSORS_ONLN)
 fi
 
 if [ -z $ANDROID_BUILD_TOP ]; then
-	echo "Please 'source build/envsetup.sh' and run 'lunch' first"
-	exit
+    echo "Please 'source build/envsetup.sh' and run 'lunch' first"
+    exit
 fi
 
-if [ $TARGET_PRODUCT != "beagleboneblack" -a $TARGET_PRODUCT != "beagleboneblack_sd" ]; then
-	echo "Please select product beagleboneblack or beagleboneblack_sd"
-	exit
-fi
-
-if [ `javac -version |& cut -d " " -f 2 | cut -b 1-3` != "1.7" ]; then
-        echo "Missing JDK or not version 1.7"
-        exit
+if [ $TARGET_PRODUCT == "beagleboneblack_sd" -o $TARGET_PRODUCT == "beagleboneblack_emmc" ]; then
+    DEVICE_DIR=device/beagleboard/beagleboneblack
+elif [ $TARGET_PRODUCT == "jarvis_sd" -o $TARGET_PRODUCT == "jarvis_emmc" ]; then
+    DEVICE_DIR=device/esmartcontrol/jarvis
+else
+    echo "Unknown TARGET_PRODUCT: $TARGET_PRODUCT"
+    exit 1
 fi
 
 echo "Building $TARGET_PRODUCT using $CORES cpu cores"
 echo ""
 echo "Building kernel"
-cd $ANDROID_BUILD_TOP/ti-kernel
+cd $ANDROID_BUILD_TOP/bb-kernel
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
-if [ ! -d patches/a4b ]; then
-	mkdir patches/a4b
-	cp $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/config-android-4.1 patches/defconfig 
-	if [ $? != 0 ]; then echo "ERROR"; exit; fi
-	cp  $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/*.patch patches/a4b
-	patch -p1 < $ANDROID_BUILD_TOP/$DEVICE_DIR/ti-kernel-patches/0001-Add-a4b-patch.patch
-	if [ $? != 0 ]; then echo "ERROR"; exit; fi
-fi
 
 AUTO_BUILD=1 ./build_kernel.sh
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-# Append the dtb to zImage because the Android build doesn't know about dtbs
-cd KERNEL
-cat arch/arm/boot/zImage arch/arm/boot/dts/am335x-boneblack.dtb > zImage-dtb
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
+# Copy the kernel in OUT
+cp KERNEL/arch/arm/boot/zImage $ANDROID_PRODUCT_OUT/zImage
 
-cp zImage-dtb $ANDROID_BUILD_TOP/$DEVICE_DIR
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
+# HACK: Merge an overlay for the fstab. This works around the lack of
+# DTBO support
+#
+# TOOD: Use DTBO a partition! It would be better if we didn't have to
+# do tis.
+if [ -f $ANDROID_BUILD_TOP/$DEVICE_DIR/dts/$TARGET_PRODUCT.dts ]; then
+    echo "Building DTB"
+    
+    rm -vf $ANDROID_PRODUCT_OUT/am335x-boneblack-android.dtb
 
-# Grab all the kernel modules
-mkdir $ANDROID_BUILD_TOP/$DEVICE_DIR/modules
-MODULES=`find -name "*.ko"`
-for f in $MODULES; do
-	cp $f $ANDROID_BUILD_TOP/$DEVICE_DIR/modules/`basename $f`
-	if [ $? != 0 ]; then echo "ERROR"; exit; fi
-done
+    # This command was carefully worked out from kernel
+    # sources. Please be cautious when changing this.
+    (cat KERNEL/arch/arm/boot/dts/am335x-boneblack.dts && \
+         echo "#include \"$ANDROID_BUILD_TOP/$DEVICE_DIR/dts/$TARGET_PRODUCT.dts\"" ) | \
+        cpp -x assembler-with-cpp -E -undef -nostdinc -IKERNEL/include -IKERNEL/arch/arm/boot/dts | \
+        KERNEL/scripts/dtc/dtc -O dtb -@ --include KERNEL/arch/arm/boot/dts \
+                               -Wno-unit_address_vs_reg \
+                               -Wno-unit_address_format \
+                               -Wno-avoid_unnecessary_addr_size \
+                               -Wno-alias_paths \
+                               -Wno-graph_child_address \
+                               -Wno-graph_port \
+                               -Wno-unique_unit_address \
+                               -Wno-pci_device_reg \
+                               -o $ANDROID_PRODUCT_OUT/am335x-boneblack-android.dtb
 
-echo "Building device tree overlays"
-cd $ANDROID_BUILD_TOP/bb.org-overlays
-make DTC=../ti-kernel/KERNEL/scripts/dtc/dtc
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
-mkdir $ANDROID_BUILD_TOP/$DEVICE_DIR/dtbo
-cp src/arm/*.dtbo $ANDROID_BUILD_TOP/$DEVICE_DIR/dtbo
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
+fi
+    
+##
+## Build Android's U-Boot provided in external/u-boot
+##
 
 echo "Building U-Boot"
-cd $ANDROID_BUILD_TOP/u-boot
-# The Android prebuilt gcc fails to build U-Boot, so use the Linaro gcc which
-# was installed to build the ti-kernel
-. ../ti-kernel/.CC
+cd $ANDROID_BUILD_TOP/external/u-boot
+
+# Use the GCC downloaded by the kernel script.
+source $ANDROID_BUILD_TOP/bb-kernel/.CC
+
 make CROSS_COMPILE=$CC am335x_evm_config
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
 make CROSS_COMPILE=$CC
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
 cd $ANDROID_BUILD_TOP
+
+#
+# Build Android sources.
+# 
 
 echo "Building Android"
 
--- write-sdcard-beagleboneblack.sh
+++ write-sdcard-beagleboneblack.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DEVICE_DIR=device/beagleboard/beagleboneblack
-
 function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 
 # Format an SD card for Android on BeagelBone Black
@@ -13,11 +11,6 @@ if [ $# -ne 1 ]; then
 fi
 
 DRIVE=$1
-
-if [ x$TARGET_PRODUCT != "xbeagleboneblack" -a x$TARGET_PRODUCT != "xbeagleboneblack_sd" ]; then
-	echo "Please run 'lunch' and select beagleboneblack or beagleboneblack_sd"
-	exit
-fi
 
 # Check the drive exists in /sys/block
 if [ ! -e /sys/block/${DRIVE}/size ]; then
@@ -47,85 +40,85 @@ else
 	CACHE_PART=/dev/${DRIVE}4
 fi
 
+set -e
+
 # Overwite existing partiton table with zeros
 sudo dd if=/dev/zero of=/dev/${DRIVE} bs=1M count=10
 if [ $? -ne 0 ]; then echo "Error: dd"; exit 1; fi
 
-# Create 4 primary partitons on the sd card
+# Create 5 primary partitons on the sd card
 #  1: boot:   FAT32, 64 MiB, boot flag
-#  2: system: Linux, 512 MiB
+#  2: system: Linux, 1024 MiB
 #  3: data:   Linux, 3072 MiB
 #  4: cache:  Linux, 256 MiB
+#  5: vendor: Linux, 10 MiB
 
 # Note that the formatting of parameters changed slightly v2.26
 SFDISK_VERSION=`sfdisk --version | awk '{print $4}'`
 if version_gt $SFDISK_VERSION "2.26"; then
      echo "sfdisk uses new syntax"
-	sudo sfdisk /dev/${DRIVE} << EOF
-,64M,0x0c,*
-,512M,,,
-,3072M,,,
+	sudo sfdisk --force /dev/${DRIVE} << EOF
+,64M,0x0C,*,
+,1024M,,,
+,2048M,,,
 ,256M,,,
 EOF
 else
-	sudo sfdisk --unit M /dev/${DRIVE} << EOF
+	sudo sfdisk --force --unit M /dev/${DRIVE} << EOF
 ,64,0x0c,*
-,512,,,
-,3072,,,
+,1024,,,
+,2048,,,
 ,256,,,
 EOF
 fi
 if [ $? -ne 0 ]; then echo "Error: sdfisk"; exit 1; fi
 
-# Format p1 with FAT32
-sudo mkfs.vfat -F 16 -n boot ${BOOT_PART}
-if [ $? -ne 0 ]; then echo "Error: mkfs.vfat"; exit 1; fi
-
+sudo mkfs.vfat -nboot ${BOOT_PART}
+if [ $? -ne 0 ]; then echo "Error: mkfs.ext4"; exit 1; fi
 
 # Copy boot files
 echo "Mounting $BOOT_PART"
 sudo mount $BOOT_PART /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
+UNAME=4.19.59-bone36
+
+sudo mkdir -p /mnt/boot/dtbs/$UNAME
+
 mkimage -A arm -O linux -T ramdisk -d ${ANDROID_PRODUCT_OUT}/ramdisk.img uRamdisk
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-sudo cp u-boot/MLO /mnt
+sudo cp external/u-boot/MLO /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-sudo cp u-boot/u-boot.img /mnt
+sudo cp external/u-boot/u-boot.img /mnt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-sudo cp uRamdisk /mnt
+sudo cp uRamdisk /mnt/boot/initrd.img-$UNAME
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-sudo cp ${DEVICE_DIR}/uEnv.txt /mnt
+sudo cp ${ANDROID_PRODUCT_OUT}/uEnv.txt /mnt/boot/
+sudo mkenvimage -s 1024 -o/mnt/boot/uboot.env ${ANDROID_PRODUCT_OUT}/uEnv.txt
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
-# Generate uImage with dtb for HDMI screen
-cat ti-kernel/KERNEL/arch/arm/boot/zImage ti-kernel/KERNEL/arch/arm/boot/dts/am335x-boneblack-hdmi-overlay.dtb > zImage-dtb-hdmi
-ti-kernel/KERNEL/scripts/mkuboot.sh -A arm -O linux -C none  -T kernel -a 0x80008000 -e 0x80008000 -n 'Linux-4.1' -d zImage-dtb-hdmi uImage-dtb-hdmi
+sudo cp $ANDROID_PRODUCT_OUT/am335x-boneblack-android.dtb /mnt/boot/dtbs/$UNAME
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
-# Generate uImage with dtb for eMMC/LCD cape
-cat ti-kernel/KERNEL/arch/arm/boot/zImage ti-kernel/KERNEL/arch/arm/boot/dts/am335x-boneblack-emmc-overlay.dtb > zImage-dtb-emmc
-ti-kernel/KERNEL/scripts/mkuboot.sh -A arm -O linux -C none  -T kernel -a 0x80008000 -e 0x80008000 -n 'Linux-4.1' -d zImage-dtb-emmc uImage-dtb-emmc
-if [ $? != 0 ]; then echo "ERROR"; exit; fi
-
-sudo cp uImage-dtb-hdmi /mnt/uImage
+sudo cp $ANDROID_PRODUCT_OUT/zImage /mnt/boot/vmlinuz-$UNAME
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 sudo umount /mnt
 
 # Copy disk images
+
 echo "Writing system"
-sudo dd if=${ANDROID_PRODUCT_OUT}/system.img of=$SYSTEM_PART bs=1M
+sudo dd if=${ANDROID_PRODUCT_OUT}/system.img of=$SYSTEM_PART bs=1M status=progress && sync
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 sudo e2label $SYSTEM_PART system
+
 echo "Writing userdata (takes a long time)"
-sudo dd if=${ANDROID_PRODUCT_OUT}/userdata.img of=$USER_PART bs=1M
+sudo dd if=${ANDROID_PRODUCT_OUT}/userdata.img of=$USER_PART bs=1M status=progress && sync
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 sudo e2label $USER_PART userdata
+
 echo "Writing cache"
-sudo dd if=${ANDROID_PRODUCT_OUT}/cache.img of=$CACHE_PART bs=1M
+sudo dd if=${ANDROID_PRODUCT_OUT}/cache.img of=$CACHE_PART bs=1M status=progress && sync
 if [ $? != 0 ]; then echo "ERROR"; exit; fi
 sudo e2label $CACHE_PART cache
 
-echo "SUCCESS! Andrdoid4Beagle installed on the uSD card. Enjoy"
+echo "SUCCESS! Android4Beagle installed on the uSD card. Enjoy"
 
 exit 0
